56dc54273508c4eba72243c10146d08ec571fe3e
Joshua Beitler <hello@joshbeitler.com>
2015-11-13 20:01:41 -0700

    What time is it?

--- app/public/javascripts/app.js
+++ app/public/javascripts/app.js
@@ -64,6 +64,43 @@
       templateUrl: "/views/app.ejs",
       controller: function($scope) {
         $scope.$on('$viewContentLoaded', function(event) {
+          function getDateTime() {
+            var now = new Date();
+            var year = now.getFullYear();
+            var month = now.getMonth() + 1;
+            var day = now.getDate();
+            var hour = now.getHours();
+            var minute = now.getMinutes();
+            var second = now.getSeconds();
+            if (month.toString().length == 1) {
+              var month = '0' + month;
+            }
+            if (day.toString().length == 1) {
+              var day = '0' + day;
+            }
+            if (hour.toString().length == 1) {
+              var hour = '0' + hour;
+            }
+            if (hour > 12) {
+              hour -= 12;
+            } else if (hour === 0) {
+              hour = 12;
+            }
+            if (minute.toString().length == 1) {
+              var minute = '0' + minute;
+            }
+            if (second.toString().length == 1) {
+              var second = '0' + second;
+            }
+            var dateTime = year + '/' + month + '/' + day + ' ' +
+              hour + ':' + minute + ':' + second;
+            return dateTime;
+          }
+
+          setInterval(function() {
+            $('#time>span').text(getDateTime());
+          }, 1000);
+
           $('.chores').on("scroll", function() {
             if ($(this).scrollTop() > 100) {
               $(this).parent().find("header").addClass(
